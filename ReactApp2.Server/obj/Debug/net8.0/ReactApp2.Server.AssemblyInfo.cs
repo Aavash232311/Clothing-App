10b28bd8006f4531b5b15d6db16e94a4c7507863
Arpan232311 <arpanbhai20@gmail.com>
2024-06-13 17:47:23 +0545

    login stack import error mui

--- ReactApp2.Server/obj/Debug/net8.0/ReactApp2.Server.AssemblyInfo.cs
+++ ReactApp2.Server/obj/Debug/net8.0/ReactApp2.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0632f292261e4c3a30f8f5a905d26ede49d7ed1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9371731cd36924b69c75b92e9fb22abd1fea1216")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
