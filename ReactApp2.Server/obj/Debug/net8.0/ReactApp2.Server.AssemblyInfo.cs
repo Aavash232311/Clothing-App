103b284989e8ea1754cc4458203fb34ed9a84ef4
Avash Lamichhane <21556@globalcollege.edu.np>
2024-06-14 20:53:48 +0545

    fixed css more.css

--- ReactApp2.Server/obj/Debug/net8.0/ReactApp2.Server.AssemblyInfo.cs
+++ ReactApp2.Server/obj/Debug/net8.0/ReactApp2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d25d580d4c5648a183753792fef9008a7656a6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c1702b3965f1a97fa57a6bd6d70b54e320c14f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
