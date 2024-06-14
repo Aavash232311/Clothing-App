481411b299dffc04e14ed8f70b67d13338b72a36
Avash Lamichhane <21556@globalcollege.edu.np>
2024-06-14 20:49:10 +0545

    fix sort error

--- reactapp2.client/src/Admin/public/More.jsx
+++ reactapp2.client/src/Admin/public/More.jsx
@@ -6,10 +6,11 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import { NavItem, NavLink } from "reactstrap";
+import { Link } from "react-router-dom";
 
 class Items extends Component {
   constructor(props) {
@@ -23,13 +24,22 @@ class Items extends Component {
       <>
         <div className="container-product-options">
           {this.props.products.value.map((i, j) => {
+            let l = i;
             return (
-              <div className="item-options" key={Math.random(0, 1000) + i.id}>
-                {i.name}
-                <br />
-                {i.price}
-                <br />
-                {i.gender}
+              <div key={Math.random(0, 1000) + i.id}>
+                <NavItem>
+                  <NavLink tag={Link} to={`/view?key=${l.id}`}>
+                    <div className="item-options">
+                      <img width="95%" height="350" src={l.images[0]}></img>
+                      <hr style={{ visibility: "hidden" }}></hr>
+                      <div className="product-cart-label-name">{l.name}</div>
+                      <hr style={{ visibility: "hidden" }}></hr>
+                      <div className="product-cart-label-name">
+                        Rs: {l.price}
+                      </div>
+                    </div>
+                  </NavLink>
+                </NavItem>
               </div>
             );
           })}
@@ -62,6 +72,7 @@ class More extends Component {
     this.SortByPrice = this.SortByPrice.bind(this);
     this.getIterableDataProductRender =
       this.getIterableDataProductRender.bind(this);
+    this.sortByDate = this.sortByDate.bind(this);
   }
 
   fetchInitialData() {
@@ -78,6 +89,7 @@ class More extends Component {
       .then((r) => r.json())
       .then((response) => {
         const { statusCode, value } = response;
+        console.log(response);
         if (statusCode === 200) {
           this.setState({ products: value, orginalProduct: value });
         }
@@ -105,33 +117,43 @@ class More extends Component {
           this.setState({ category: value });
         }
       });
-    this.fetchInitialData(); 
+    this.fetchInitialData();
   }
 
   setDropDown(value) {
     // re-render all we don-t want the combied logic mess
-    const arr = ["sales", "price", "recentlyAdded", "gender"];
+    const arr = ["sales", "price", "recentlyAdded", "gender"]; // all state name that represents a dropdown
     arr.map((i) => {
       if (i !== value) {
-        this.setState({[i]: false});
+        this.setState({ [i]: false });
       }
-    })
+    });
     if (this.state[value] === true) {
       this.setState({ [value]: false });
       return;
     }
     this.setState({ [value]: true });
   }
-  bubbleSort(arr, cd) {
+  bubbleSort(arr, cd, compare) {
     for (let i = 0; i < arr.length - 1; i++) {
       for (let j = 0; j < arr.length - i - 1; j++) {
-        if (arr[j].price > arr[j + 1].price) {
+        let c1, c2;
+        if (cd === "date") {
+          // we want to fist convert into readable javascript date
+          c1 = new Date(arr[j][compare]).getTime();
+          c2 = new Date(arr[j + 1][compare]).getTime();
+        } else {
+          c1 = arr[j][compare];
+          c2 = arr[j + 1][compare];
+        }
+        if (c1 > c2) {
           const temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
         }
       }
     }
+    if (cd === "date") return arr;
     if (cd === "-ve") {
       let temp = [];
       for (let i = arr.length - 1; i >= 0; i--) {
@@ -148,9 +170,9 @@ class More extends Component {
     this.setState({ priceOptions: value, SortByGender: "all" });
     console.log(FILTER_PRODUCT);
     if (value === "lowToHigh") {
-      FILTER_PRODUCT = this.bubbleSort(FILTER_PRODUCT, "+ve"); // because we might want to filter among other selected category
+      FILTER_PRODUCT = this.bubbleSort(ORIGIN, "+ve", "price"); // because we might want to filter among other selected category
     } else if (value === "highToLow") {
-      FILTER_PRODUCT = this.bubbleSort(FILTER_PRODUCT, "-ve");
+      FILTER_PRODUCT = this.bubbleSort(ORIGIN, "-ve", "price");
     }
     this.setState({ products: { value: FILTER_PRODUCT, page: PAGE } });
   }
@@ -169,11 +191,24 @@ class More extends Component {
     const { value } = ev.target;
     let { PAGE, ORIGIN, FILTER_PRODUCT } = this.getIterableDataProductRender();
     if (value === "all") {
-      FILTER_PRODUCT = FILTER_PRODUCT.filter((x) => true);
-    }else{
-      FILTER_PRODUCT = FILTER_PRODUCT.filter((x) => x.gender === value);
+      FILTER_PRODUCT = ORIGIN.filter((x) => true);
+    } else {
+      FILTER_PRODUCT = ORIGIN.filter((x) => x.gender === value);
     }
     this.setState({ products: { value: FILTER_PRODUCT, page: PAGE } });
+  }
+
+  sortByDate(ev) {
+    const { checked } = ev.target;
+    let { PAGE, ORIGIN, FILTER_PRODUCT } = this.getIterableDataProductRender();
+    let sortOnTheBasisOfDate;
+    if (checked === true) {
+      sortOnTheBasisOfDate = this.bubbleSort(FILTER_PRODUCT, "date", "added"); // by default the server returns on the basis of sorted data
+      // but when we filter on the basis of gender let's say it sorts recently added
+    } else {
+      sortOnTheBasisOfDate = ORIGIN;
+    }
+    this.setState({ products: { value: sortOnTheBasisOfDate, page: PAGE } });
   }
 
   render() {
@@ -198,7 +233,7 @@ class More extends Component {
                       grid.style.gridTemplateColumns = "100%";
                       this.setState({ slowFilter: false });
                     } else {
-                      grid.style.gridTemplateColumns = "10% 100%";
+                      grid.style.gridTemplateColumns = "15% 82%";
                       this.setState({ slowFilter: true });
                     }
                   }}
@@ -276,37 +311,6 @@ class More extends Component {
                             className="classic-label options-more"
                             style={{ float: "left" }}
                             onClick={() => {
-                              this.setDropDown("sales");
-                            }}
-                          >
-                            <div>Sales</div>
-                            <div className="arrow-up-options">
-                              {this.state.sales === false ? (
-                                <KeyboardArrowUpIcon />
-                              ) : (
-                                <KeyboardArrowDownIcon />
-                              )}
-                            </div>
-                          </div>
-                        </center>
-                        <hr style={{ visibility: "hidden", height: "50px" }} />
-                        {this.state.sales === true ? (
-                          <>
-                            <div className="options-dropdown">
-                              <FormGroup>
-                                <FormControlLabel
-                                  control={<Checkbox />}
-                                  label="Sales"
-                                />
-                              </FormGroup>
-                            </div>
-                          </>
-                        ) : null}
-                        <center>
-                          <div
-                            className="classic-label options-more"
-                            style={{ float: "left" }}
-                            onClick={() => {
                               this.setDropDown("price");
                             }}
                           >
@@ -361,20 +365,27 @@ class More extends Component {
                                 <KeyboardArrowDownIcon />
                               )}
                             </div>
-                            {this.state.recentlyAdded === true ? (
-                              <>
-                                <div className="options-dropdown">
-                                  <FormGroup>
-                                    <FormControlLabel
-                                      control={<Checkbox />}
-                                      label="recent"
-                                    />
-                                  </FormGroup>
-                                </div>
-                              </>
-                            ) : null}
                           </div>
                         </center>
+                        <hr style={{ visibility: "hidden", height: "50px" }} />
+                        {this.state.recentlyAdded === true ? (
+                          <>
+                            <div className="options-dropdown">
+                              <FormGroup style={{ float: "left" }}>
+                                <FormControlLabel
+                                  control={
+                                    <Checkbox
+                                      onChange={(ev) => {
+                                        this.sortByDate(ev);
+                                      }}
+                                    />
+                                  }
+                                  label="recent"
+                                />
+                              </FormGroup>
+                            </div>
+                          </>
+                        ) : null}
                       </div>
                     </>
                   ) : null}
